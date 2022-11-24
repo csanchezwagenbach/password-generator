418af2fd3407e74b61eaee06a8674894415e72e9
Colby Sanchez Wagenbach <csanchezwagenbach@gmail.com>
2022-11-24 14:01:55 -0800

    created lengthPrompt function in js file

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var lengthInput;
+var passwordLength;
+
+function lengthPrompt() {
+  lengthInput = prompt("How many characters would you like in your random password?", "Enter an integer between 8 and 128");
+  passwordLength = Number(lengthInput);
+  console.log(lengthInput);
+  console.log(passwordLength);
+  console.log(typeof passwordLength);
+  if ((passwordLength < 8) || (passwordLength > 128) || (!passwordLength)) {
+    alert("You must enter an integer between 8 and 128!")
+    lengthPrompt();
+} else {confirm("Your randomly generated password will be " + passwordLength + " characters long.")}
+}
+  
+
+
+function generatePassword() {
+  lengthPrompt();
+  console.log(passwordLength);
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
